e7e92fbf3004ce7db22ee44ff212901edd629fb2
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-10 01:14:07 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,9 +1,7 @@
 def my_select(collection)
   i = 0
-  new_arr = []
   while i < collection.length
-    new_arr << yield(collection[i])
+    yield(collection[i])
     i = i + 1
   end
-  new_arr
 end
